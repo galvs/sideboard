f52b77b4ae7c1dedad75e4e263109cde4876625e
Patrick Galvin <patrick@vaio.(none)>
2010-08-29 21:04:00 +0100

    Added hiccup support.

--- src/main/clojure/sideboard/webfunctions.clj
+++ src/main/clojure/sideboard/webfunctions.clj
@@ -4,6 +4,7 @@
    couchdb.client
    clojure.contrib.json
    [clout.core :as clout]
+   [hiccup.core :as hiccup]
    )
   )
 
@@ -18,13 +19,13 @@
 (defn ^{web/uri (fn [uri] (match-document-route uri))
         } document-html [context]
           (let [docid (get (match-document-route (get-in context [:request :uri])) "docid")]
-            (str
-             (:value ( first (:rows (couchdb.client/view-get "http://localhost:5984/" "sideboard" "pages" "pages"))))
-             "<p>The document id was " docid "</p>"
-             )
+            (hiccup/html
+             [:h1 "Document " docid]
+             [:h2 "Content"]
+             [:p
+              (:value (first (:rows (couchdb.client/view-get "http://localhost:5984/" "sideboard" "pages" "pages"))))])
             )
-  
-)
+          )
 
 (defn ^{web/uri "/pat.html"} pat-html [context]
   "<h1>Hello  - this is the patrick page</h1>")
