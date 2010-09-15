846165c3d53c00a832abc0d6ccd99630e8a8f017
Malcolm Sparks <malcolm@congreve.com>
2010-09-15 08:26:22 +0100

    Upgraded to latest version of plugboard.

--- src/sideboard/main.clj
+++ src/sideboard/main.clj
@@ -2,7 +2,6 @@
   [:require
    ring.adapter.jetty
    [plugboard.core.plugboard :as plugboard]
-   [plugboard.core.configurations :as pc]
    [plugboard.webfunction.webfunction :as web]
    (plugboard.webfunction selectors plugboards response)
    sideboard.webfunctions]
@@ -11,9 +10,10 @@
 (defn application-handler [req]
   (plugboard.webfunction.response/get-response
    req
-   (merge pc/default-decision-map
-          (plugboard.webfunction.plugboards/web-function-resources
-           (map find-ns ['sideboard.webfunctions]))
-          )
+   (plugboard/merge-plugboards
+    plugboard/default-decision-map
+    (plugboard.webfunction.plugboards/web-function-resources
+     (map find-ns ['sideboard.webfunctions]))
+    )
    ))
 
